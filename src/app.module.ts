f8d5f2ae08626d43f27b8a0b02f7ec26c7bddc89
Sebastianus Sembara <sembara9090@gmail.com>
2022-03-08 16:28:06 +0700

    feat: config port in .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './common/config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MovieModule, TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    MovieModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  console.log(`Apps is running is port: ${port}`);
+
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
