eb56a5f561ba7a601e2398062e03497b35b6907d
Sebastianus Sembara <sembara9090@gmail.com>
2022-03-08 17:12:58 +0700

    feat: enable versioning URI

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,7 +10,10 @@ async function bootstrap() {
   const port = configService.get('port');
   console.log(`Apps is running is port: ${port}`);
 
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(port);
 }
 bootstrap();
--- src/movie/controllers/movie.controller.ts
+++ src/movie/controllers/movie.controller.ts
@@ -1,3 +1,6 @@
+import { MovieEntity } from '../entities/movie.entity';
+import { MoviesDto } from '../dtos/store.movie.dto';
+import { MovieService } from '../services/movie.service';
 import {
   Body,
   Controller,
@@ -6,30 +9,32 @@ import {
   Param,
   Post,
   Put,
+  Version,
 } from '@nestjs/common';
-import { MoviesDto } from '../dtos/store.movie.dto';
-import { MovieEntity } from '../entities/movie.entity';
-import { MovieService } from '../services/movie.service';
 
 @Controller('movies')
 export class MovieController {
   constructor(private _moviesService: MovieService) {}
 
+  @Version('1')
   @Get()
   public async getMovies(): Promise<MovieEntity[]> {
     return this._moviesService.getMovies();
   }
 
+  @Version('1')
   @Get(':id')
   getById(@Param() params): Promise<MovieEntity> {
     return this._moviesService.getById(params.id);
   }
 
+  @Version('1')
   @Post()
   public async storeMovies(@Body() data: MoviesDto): Promise<MovieEntity> {
     return this._moviesService.storeMovies(data);
   }
 
+  @Version('1')
   @Put(':id')
   public async updateMovies(
     @Param() params,
@@ -38,6 +43,7 @@ export class MovieController {
     return this._moviesService.updateMovies(params.id, data);
   }
 
+  @Version('1')
   @Delete(':id')
   public async destroyMovies(@Param() params): Promise<void> {
     return this._moviesService.destroyMovies(params.id);
