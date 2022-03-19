68bed9c37f6e3f32214c6b866742eb30616ca1fd
Sebastianus Sembara <sembara9090@gmail.com>
2022-03-19 14:31:23 +0700

    fix: validation more readeble

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
+/* eslint-disable @typescript-eslint/ban-types */
 import {
   PipeTransform,
   Injectable,
@@ -16,16 +16,44 @@ export class ValidationPipe implements PipeTransform<any> {
     }
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
-    if (errors.length > 0) {
-      console.log(errors);
-
-      throw new BadRequestException('Validation failed');
+    const { length: errorsLength } = errors;
+    if (errorsLength > 0) {
+      this.handleErrors(errors, errorsLength);
     }
     return value;
+  }
+
+  private handleErrors(errors: ValidationError[], errorsLength: number) {
+    const errorsMessage = this.getErrorsMessage(errors, errorsLength);
+
+    throw new BadRequestException(errorsMessage);
   }
 
   private toValidate(metatype: Function): boolean {
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
+  }
+
+  private camelToSnakeCase(str) {
+    return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+  }
+
+  private getErrorsMessage(errors, errorsLength) {
+    const emptyString = '';
+    const errorsMessages = errors.reduce(
+      (stringAccumulator, errorObject, index) => {
+        const key = Object.keys(errorObject.constraints)[0];
+        const errorMessageArray = errorObject.constraints[key].split(' ');
+        const firstWord = errorMessageArray[0];
+        errorMessageArray[0] = this.camelToSnakeCase(firstWord);
+        const prefix = index === 0 ? '' : ' ';
+        const suffix = index === errorsLength - 1 ? '.' : ',';
+        return (stringAccumulator += `${prefix}${errorMessageArray.join(
+          ' ',
+        )}${suffix}`);
+      },
+      emptyString,
+    );
+    return errorsMessages;
   }
 }
